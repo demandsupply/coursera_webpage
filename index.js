d2671b1c4dd4ea5d4354ddf613b380663f9813e5
demandsupply <giova.zol92@gmail.com>
2023-07-31 19:31:39 +0200

    created the js function used to made work the carousel 4b and 4c

--- index.js
+++ index.js
@@ -73,44 +73,49 @@ function shoWindow(id) {
 
 // Create the carousel function
 
-const buttonPrev = document.querySelector('[data-carousel-button-left]');
-const buttonNext = document.querySelector('[data-carousel-button-right]');
-const slidesContainer = document.querySelector('[data-slides]')
+function setUpCarousel(carousel) {
+    const buttonPrev = carousel.querySelector('[data-carousel-button-left]');
+    const buttonNext = carousel.querySelector('[data-carousel-button-right]');
+    const slidesContainer = carousel.querySelector('[data-slides]')
 
 
-buttonNext.addEventListener('click', handleNext);
-buttonPrev.addEventListener('click', handlePrev);
+    buttonNext.addEventListener('click', handleNext);
+    buttonPrev.addEventListener('click', handlePrev);
 
-buttonNext.addEventListener('click', () => console.log('well done!'));
+    buttonNext.addEventListener('click', () => console.log('well done!'));
 
-// slidesContainer.style.transform = 'translate(-50%)';
+    // slidesContainer.style.transform = 'translate(-50%)';
 
-let currentSlide = 0;
-const numSlides = (slidesContainer.children.length/4 -1);
+    let currentSlide = 0;
+    const numSlides = (slidesContainer.children.length/4 -1);
 
-function handlePrev() {
-    if (currentSlide < 0) {
-        currentSlide = currentSlide;
+    function handlePrev() {
+        if (currentSlide < 0) {
+            currentSlide = currentSlide;
+        }
+        else if (currentSlide > 0) {
+            currentSlide = (currentSlide -1) % numSlides;
+            slidesContainer.style.transform = `translateX(${currentSlide * -102.5}%)`;
+        }
     }
-    else if (currentSlide > 0) {
-        currentSlide = (currentSlide -1) % numSlides;
-        slidesContainer.style.transform = `translateX(${currentSlide * -102.5}%)`;
+
+    function handleNext() {
+        if (currentSlide >= numSlides) {
+            currentSlide = currentSlide;
+        }
+        else if (currentSlide < numSlides) {
+            currentSlide = currentSlide + 1;
+            // slidesContainer.style.transform = `translateX(${currentSlide * -(200/(slidesContainer.children.length))}%)`;
+            slidesContainer.style.transform = `translateX(${currentSlide * -102.5}%)`;
+        }
     }
 }
 
-function handleNext() {
-    if (currentSlide >= numSlides) {
-        currentSlide = currentSlide;
-    }
-    else if (currentSlide < numSlides) {
-        currentSlide = currentSlide + 1;
-        // slidesContainer.style.transform = `translateX(${currentSlide * -(200/(slidesContainer.children.length))}%)`;
-        slidesContainer.style.transform = `translateX(${currentSlide * -102.5}%)`;
-    }
-}
+// Create a variable [array] which contains all the carousels of the page 
+const carouselAll = document.querySelectorAll('[data-carousel]');
 
-
-
+// Apply the function to each carousel of the page 
+carouselAll.forEach(setUpCarousel);
 
 
 // Connect the dots
